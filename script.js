8beb95aceb0044ab3d6558b82fdf817bba5bba2f
xayrullo <xayrulloxalilov0010@gmail.com>
2022-04-08 14:46:21 +0500

    bitrinci js

---
+++ script.js
@@ -0,0 +1,22 @@
+let ism = prompt('ismingizni kiriting')
+let yosh = prompt('yoshingizni kiriting')
+let odam = prompt('misolni yechin 17+9')
+let keyin = prompt('misolni yechin 56*9')
+let hello = prompt('misolni yechin 6*10')
+let kettik = prompt('misolni yechin 25*8');
+let kim = prompt('misolni yechin 78*2');
+let world = prompt('misolni yechin 19*9');
+let school = prompt('misolni yechin 25*7');
+let index = prompt('misolni yechin 39*4');
+alert ("javobni ko'rish uchun ko'nsolga kiring")
+
+console.log('sizning ismingiz ' + ism)
+console.log('sizning yosh ' + yosh);
+console.log('sizning javob 17+9 =26 togri javob:' + odam);
+console.log('sizning javob 56*9 =504 togri javob:' + keyin);
+console.log('sizning javob 6*10 =60 togri javob:' + hello);
+console.log('sizning javob 25*8 =200 togri javob:' + kettik);
+console.log('sizning javob 78*2 =156 togri javob:' + kim);
+console.log('sizning javob 19*9 =171 togri javob:' + world);
+console.log('sizning javob 25*7 =175 togri javob:' + school);
+console.log('sizning javob 39*4 =156 togri javob:' + index);
